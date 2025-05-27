7ac7eadb946ab8582921c7e8559909e1c3e32ff6
Isak Samsten <isak@samsten.se>
2025-05-28 00:15:22 +0200

    initial import

---
+++ src/klar/__init__.py
@@ -0,0 +1,521 @@
+from ctypes import CDLL
+from operator import le
+from sqlite3 import connect
+from typing import Iterable, override
+
+CDLL("libgtk4-layer-shell.so.0")
+import gi
+
+gi.require_version("Gtk4LayerShell", "1.0")
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+
+import threading
+import time
+
+import pulsectl
+
+from ._config import (
+    DEFAULT_BRIGHTNESS_PROVIDER,
+    DEFAULT_CSS_PROVIDER,
+    DEFAULT_USER_CSS_PROVIDER,
+    DEFAULT_KEYBOARD_BRIGHTNESS_PROVIDER,
+    BrightnessProvider,
+)
+
+
+from gi.repository import Adw, Gdk, Gio, GLib, GObject, Gtk
+from gi.repository import Gtk4LayerShell as LayerShell
+
+
+class Monitor(GObject.Object):
+    def start(self):
+        self.do_start()
+
+    def close(self):
+        self.do_close()
+
+    def is_started(self):
+        return False
+
+    def do_close(self):
+        raise NotImplementedError()
+
+    def do_start(self):
+        raise NotImplementedError()
+
+    def new_model(self):
+        raise NotImplementedError()
+
+
+class FileMonitor(Monitor):
+    def __init__(self, file: Gio.File) -> None:
+        super().__init__()
+        self._file = file
+        self._file_monitor = None
+        self._file_monitor_id = None
+
+    @override
+    def do_start(self):
+        self._file_monitor = self._file.monitor(Gio.FileMonitorFlags.NONE, None)
+        self._file_monitor_id = self._file_monitor.connect(
+            "changed", self._on_file_change
+        )
+
+    @override
+    def is_started(self):
+        return self._file_monitor_id is not None
+
+    @override
+    def do_stop(self):
+        if self._file_monitor_id is not None:
+            self._file_monitor.cancel()
+            self._file_monitor.disconnect(self._file_monitor_id)
+            self._file_monitor_id = None
+
+    def _on_file_change(self, monitor, file, other_file, event_type):
+        if event_type == Gio.FileMonitorEvent.CHANGES_DONE_HINT:
+            with open(file) as file:
+                self.on_change(file.read())
+
+    def on_change(self, content: str):
+        pass
+
+
+class BrightnessMonitor(FileMonitor):
+    brightness = GObject.Property(type=float, default=0.0)
+
+    def __init__(self, icon, brightness_provider: BrightnessProvider):
+        super().__init__(Gio.File.new_for_path(brightness_provider.brightness_file))
+        self.icon = icon
+        self.brightness_provider = brightness_provider
+
+    @override
+    def on_change(self, data: str):
+        self.brightness = int(data) / self.brightness_provider.max_brightness
+
+    def new_model(self):
+        if not self.is_started():
+            raise ValueError(
+                f"BrightnessMonitor for {self.brightness_provider.brightness_file}"
+                " has not been started"
+            )
+        model = StatusModel(f"brightness-{self.brightness_provider.source}", 16)
+        model.icon = Gio.ThemedIcon.new(self.icon)
+        self.bind_property("brightness", model, "value")
+        return model
+
+
+def find_default_ac_path():
+    upower_proxy = Gio.DBusProxy.new_for_bus_sync(
+        Gio.BusType.SYSTEM,
+        Gio.DBusProxyFlags.NONE,
+        None,
+        # Service name, object path, interface:
+        "org.freedesktop.UPower",
+        "/org/freedesktop/UPower",
+        "org.freedesktop.UPower",
+        None,
+    )
+
+    result = upower_proxy.call_sync(
+        "EnumerateDevices", None, Gio.DBusCallFlags.NONE, -1, None
+    )
+
+    for obj_path in result.unpack()[0]:
+        device_proxy = Gio.DBusProxy.new_for_bus_sync(
+            Gio.BusType.SYSTEM,
+            Gio.DBusProxyFlags.NONE,
+            None,
+            "org.freedesktop.UPower",
+            obj_path,
+            "org.freedesktop.UPower.Device",
+            None,
+        )
+        typ = device_proxy.get_cached_property("Type")
+        if typ is not None and typ.unpack() == 1:
+            return device_proxy
+    return None
+
+
+class AcMonitor(Monitor):
+    connected = GObject.Property(type=bool, default=False)
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._dbus_proxy = find_default_ac_path()
+        self._dbus_proxy_id = None
+
+    @override
+    def do_start(self):
+        self._dbus_proxy_id = self._dbus_proxy.connect(
+            "g-properties-changed", self._on_properties_changed
+        )
+
+    @override
+    def is_started(self):
+        return self._dbus_proxy_id is not None
+
+    @override
+    def do_close(self):
+        self._dbus_proxy.disconnect(self._dbus_proxy_id)
+
+    def _on_properties_changed(self, proxy, changed, invalidated):
+        if "Online" in changed.keys():
+            self.connected = changed["Online"]
+
+    @override
+    def new_model(self):
+        if not self.is_started():
+            raise ValueError()
+
+        model = StatusModel("ac", levels=0)
+
+        def update_icon(binding, prop):
+            if self.connected:
+                icon_name = "battery-level-50-charging-symbolic"
+            else:
+                icon_name = "battery-level-50-symbolic"
+            model.icon = Gio.ThemedIcon.new_with_default_fallbacks(icon_name)
+
+        self.connect("notify::connected", update_icon)
+        return model
+
+
+class PulseAudioMonitor(Monitor):
+    volume = GObject.Property(type=float, default=0.0)
+    mute = GObject.Property(type=bool, default=False)
+    current_sink = GObject.Property(type=str, default="")
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._timers_lock = threading.Lock()
+        self._timers = {}
+        self._pulse_listen = pulsectl.Pulse(
+            "klar-pulse-daemon-listen", threading_lock=True, connect=False
+        )
+        self._pulse_query = pulsectl.Pulse(
+            "klar-pulse-daemon-query", threading_lock=True, connect=False
+        )
+
+        self._pulse_event_listen_thread = threading.Thread(
+            None, self._pulse_listen.event_listen, daemon=True
+        )
+
+    def do_start(self):
+        self._pulse_listen.connect()
+        self._pulse_query.connect()
+        self._pulse_listen.event_mask_set("sink", "source")
+        self._pulse_listen.event_callback_set(self._on_pulse_event_change)
+        self._pulse_event_listen_thread.start()
+
+    def do_stop(self):
+        self._pulse_listen.close()
+        self._pulse_query.close()
+        self._pulse_event_listen_thread.join()
+
+    def _on_pulse_event_change(self, ev):
+        with self._timers_lock:
+            if index := self._timers.get(ev.index):
+                GLib.source_remove(index)
+            self._timers[ev.index] = GLib.timeout_add(
+                20, self._on_pulse_event_timeout, ev.index
+            )
+
+    def _on_pulse_event_timeout(self, index):
+        try:
+            with self._timers_lock:
+                del self._timers[index]
+
+            sink = self._pulse_query.sink_default_get()
+            volume = sink.volume
+            if self.current_sink == "" or self.mute != sink.mute:
+                self.mute = sink.mute
+
+            if self.volume != volume.value_flat:
+                self.volume = volume.value_flat
+
+            if self.current_sink != sink.name:
+                self.current_sink = sink.name
+        except Exception:
+            import logging
+
+            logging.exception("Exception in PulseDaemon._on_pulse_event_timeout")
+        return False
+
+    def new_model(self):
+        if not self._pulse_event_listen_thread.is_alive():
+            raise ValueError("PulseAudioMonitor has not been started")
+
+        model = StatusModel("pulse", 16)
+
+        def update_icon(binding, prop):
+            if self.mute:
+                icon_name = "audio-volume-muted-symbolic"
+            else:
+                icon_name = "audio-volume-high-symbolic"
+            model.icon = Gio.ThemedIcon.new_with_default_fallbacks(icon_name)
+
+        self.bind_property("volume", model, "value")
+        self.connect("notify::mute", update_icon)
+        update_icon(None, None)
+        return model
+
+
+def ease_out_cubic(t):
+    return 1 - pow(1 - t, 3)
+
+
+class GenericTransition:
+    def __init__(
+        self, method, *, before, setter, initial, target, duration=200, easing=None
+    ):
+        self._timer_id = None
+        self.initial = initial
+        self.target = target
+        self._current = None
+        self.method = method
+        self.setter = setter
+        self.before = before
+        self.duration = duration
+        self.easing = easing
+
+    def __call__(self, *args, **kwargs):
+        before = self.before(*args, **kwargs)
+        if not before:
+            initial, target = self.target, self.initial
+        else:
+            initial = self.initial
+            target = self.target
+
+        self._current = initial
+        easing = self.easing
+        if easing is None:
+            easing = ease_out_cubic
+
+        if self.duration == 0:
+            self.setter(target)
+            self.method(*args, **kwargs)
+            return
+
+        if before:
+            self.setter(initial)
+            self.method(*args, **kwargs)
+
+        def idle_add():
+            delta = target - initial
+            start_time = time.monotonic()
+
+            def do_animation():
+                elapsed = (time.monotonic() - start_time) * 1000
+                t = min(elapsed / self.duration, 1.0)
+                eased_t = easing(t)
+                self._current = initial + delta * eased_t
+                if t < 1.0:
+                    self.setter(self._current)
+                    return True
+                else:
+                    self._timer_id = None
+                    self._current = None
+                    self.setter(target)
+                    if not before:
+                        self.method(*args, **kwargs)
+                    return False
+
+            if self._timer_id is not None:
+                GLib.source_remove(self._timer_id)
+
+            self._timer_id = GLib.timeout_add(16, do_animation)
+
+        GLib.idle_add(idle_add)
+
+
+class StatusSegment(Gtk.Box):
+    def __init__(self, height=2, width=-1):
+        super().__init__(orientation=Gtk.Orientation.HORIZONTAL)
+        self.set_name("status-segment")
+        self.set_hexpand(True)
+        self.set_halign(Gtk.Align.CENTER)
+        self.set_size_request(width, height)
+
+    def set_active(self, active):
+        if active:
+            self.add_css_class("active")
+        else:
+            self.remove_css_class("active")
+
+    def set_warning(self, warning):
+        if warning:
+            self.add_css_class("warning")
+        else:
+            self.remove_css_class("warning")
+
+
+class StatusModel(GObject.Object):
+    value = GObject.Property(type=float, default=0)
+    icon = GObject.Property(type=Gio.Icon, default=None)
+    name: str
+    levels: int
+
+    def __init__(self, name: str, levels: int = 16):
+        super().__init__()
+        self.name = name
+        self.levels = levels
+
+
+class StatusBar(Gtk.Box):
+    def __init__(self, levels=10):
+        super().__init__(orientation=Gtk.Orientation.HORIZONTAL, spacing=2)
+        self.set_name("status-bar")
+        self.levels = levels
+        if levels == 0:
+            self.set_opacity(0)
+
+        for i in range(levels):
+            self.append(StatusSegment(height=10, width=8))
+
+    def __iter__(self) -> Iterable[StatusSegment]:
+        current = self.get_first_child()
+        while current is not None:
+            yield current
+            current = current.get_next_sibling()
+
+    def set_level(self, level: float):
+        filled_levels = int(round(max(0.0, min(1.0, level)) * self.levels))
+        for i, status_segment in enumerate(list(self)):
+            status_segment.set_active(i <= filled_levels)
+            status_segment.set_warning(level > 1.0)
+
+
+class StatusIndicator(Gtk.Box):
+    model: StatusModel
+
+    def __init__(
+        self,
+        *,
+        model: StatusModel,
+    ):
+        super().__init__(orientation=Gtk.Orientation.VERTICAL, name=model.name)
+        self.image = Gtk.Image.new()
+        self.image.set_pixel_size(88)
+        self.image.add_css_class("icon")
+        self.progress_bar = StatusBar(model.levels)
+        self.append(self.image)
+        self.append(self.progress_bar)
+        self._model_value_id = None
+        self._model_icon_id = None
+        self.model = model
+        self._model_value_id = self.model.connect(
+            "notify::value", lambda model, prop: self.on_value_change(model.value)
+        )
+        self._model_icon_id = self.model.connect(
+            "notify::icon", lambda model, prop: self.on_icon_change(model.icon)
+        )
+        self.on_value_change(model.value)
+        self.on_icon_change(model.icon)
+
+    def on_value_change(self, progress):
+        self.progress_bar.set_level(progress)
+
+    def on_icon_change(self, icon):
+        if icon is not None:
+            self.image.set_from_gicon(icon)
+
+
+class KlarWindow(Gtk.Window):
+    def __init__(self, app) -> None:
+        super().__init__(application=app)
+        self.set_name("klar")
+        main_view = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        main_view.set_name("main-view")
+        self.stack = Gtk.Stack(
+            hhomogeneous=True,
+            vhomogeneous=True,
+            transition_type=Gtk.StackTransitionType.CROSSFADE,
+        )
+        main_view.append(self.stack)
+        self.set_child(main_view)
+
+        self.set_visible = GenericTransition(
+            self.set_visible,
+            before=lambda x: x,
+            setter=self.set_opacity,
+            initial=0.01,
+            target=1.0,
+        )
+
+    def add_status_indicator(self, progress_box: StatusIndicator):
+        self.stack.add_named(progress_box, progress_box.get_name())
+
+    def switch_to(self, name):
+        transition_type = Gtk.StackTransitionType.NONE
+        if self.get_visible():
+            transition_type = Gtk.StackTransitionType.NONE
+
+        self.stack.set_visible_child_full(name, transition_type)
+
+
+MONITORS = [
+    PulseAudioMonitor(),
+    BrightnessMonitor("display-brightness-symbolic", DEFAULT_BRIGHTNESS_PROVIDER),
+    BrightnessMonitor(
+        "keyboard-brightness-symbolic", DEFAULT_KEYBOARD_BRIGHTNESS_PROVIDER
+    ),
+    AcMonitor(),
+]
+
+
+class KlarApp(Adw.Application):
+    def __init__(self) -> None:
+        super().__init__(application_id="se.samsten.klar")
+        self.window = None
+        self._timer_id = None
+
+    def do_activate(self):
+        def show_callback(model, prop):
+            if self._timer_id is None:
+                self.window.set_visible(True)
+            else:
+                GLib.source_remove(self._timer_id)
+
+            self.window.switch_to(model.name)
+
+            def hide():
+                self.window.set_visible(False)
+                self._timer_id = None
+                return False
+
+            self._timer_id = GLib.timeout_add(1000, hide)
+
+        self.window = KlarWindow(self)
+        for monitor in MONITORS:
+            monitor.start()
+            status_model = monitor.new_model()
+            status_model.connect("notify", show_callback)
+            status_indicator = StatusIndicator(model=status_model)
+            self.window.add_status_indicator(status_indicator)
+
+        LayerShell.init_for_window(self.window)
+        LayerShell.set_namespace(self.window, "klar")
+        LayerShell.set_layer(self.window, LayerShell.Layer.OVERLAY)
+        LayerShell.auto_exclusive_zone_enable(self.window)
+        LayerShell.set_keyboard_mode(self.window, LayerShell.KeyboardMode.NONE)
+
+
+def main():
+    klar = KlarApp()
+    Gtk.StyleContext.add_provider_for_display(
+        Gdk.Display.get_default(),
+        DEFAULT_CSS_PROVIDER,
+        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION,
+    )
+    if DEFAULT_USER_CSS_PROVIDER is not None:
+        Gtk.StyleContext.add_provider_for_display(
+            Gdk.Display.get_default(),
+            DEFAULT_USER_CSS_PROVIDER,
+            Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION + 2,
+        )
+
+    klar.register(None)
+    klar.run()
---
+++ src/klar/_config.py
@@ -0,0 +1,74 @@
+import os
+import importlib
+import importlib.resources
+from dataclasses import dataclass
+
+from gi.repository import Gtk
+
+
+def load_system_style(filename="style.css", priority=0):
+    with importlib.resources.files("klar.resources").joinpath(filename).open("rb") as f:
+        provider = Gtk.CssProvider()
+        css_data = f.read()
+        provider.load_from_data(css_data)
+        return provider
+
+
+def load_user_style(filename="style.css"):
+    config_home = os.environ.get("XDG_CONFIG_HOME", os.path.expanduser("~/.config"))
+    user_css_path = os.path.join(config_home, "klar", filename)
+    if os.path.isfile(user_css_path):
+        with open(user_css_path, "rb") as f:
+            user_provider = Gtk.CssProvider()
+            css_data = f.read()
+            user_provider.load_from_data(css_data)
+            return user_provider
+    return None
+
+
+@dataclass
+class BrightnessProvider:
+    max_brightness: int
+    brightness_file: str
+    source: str
+
+
+def guess_default_brightness_provider(
+    base="/sys/class/backlight",
+) -> BrightnessProvider:
+    subdirs = os.listdir(base)
+    if not subdirs:
+        raise FileNotFoundError("No backlight devices found!")
+
+    best = None
+    best_max = -1
+    for subdir in subdirs:
+        max_brightness_file = os.path.join(base, subdir, "max_brightness")
+        brightness_file = os.path.join(base, subdir, "brightness")
+        if os.path.isfile(max_brightness_file) and os.path.isfile(brightness_file):
+            with open(max_brightness_file) as f:
+                max_brightness = int(f.read())
+
+            if max_brightness > best_max:
+                best = BrightnessProvider(
+                    max_brightness=max_brightness,
+                    brightness_file=brightness_file,
+                    source=subdir,
+                )
+                best_max = max_brightness
+
+    if best is None:
+        raise Exception("No usable backlight device found!")
+
+    return best
+
+
+DEFAULT_BRIGHTNESS_PROVIDER = guess_default_brightness_provider()
+DEFAULT_KEYBOARD_BRIGHTNESS_PROVIDER = guess_default_brightness_provider(
+    "/sys/devices/platform/led-controller/leds/"
+)
+DEFAULT_CSS_PROVIDER = load_system_style(filename="style.css")
+DEFAULT_DARK_CSS_PROVIDER = load_system_style(filename="style-dark.css")
+
+DEFAULT_USER_CSS_PROVIDER = load_user_style(filename="style.css")
+DEFAULT_DARK_USER_CSS_PROVIDER = load_user_style(filename="style-dark.css")
